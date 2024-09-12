41d858fcad51e97f13182c34cc295db331056572
Abhishek Chauhan <abhishekashokchauhan@gmail.com>
2024-09-12 21:44:02 +0530

    Made the to do app !

---
+++ src/component/Task.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Task = ({ task, index, removeTask, completeTask }) => {
+  return (
+    <div className="border-solid border m-3 p-1 flex justify-between">
+      <li
+        className={task.completed ? "m-1 text-lg line-through" : "m-1 text-lg"}
+      >
+        {task.title}
+      </li>
+      <div className="flex">
+        <img
+          onClick={() => completeTask(index)}
+          alt="complete task"
+          className="h-6 py-auto m-1 cursor-pointer"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Eo_circle_yellow_checkmark.svg/1200px-Eo_circle_yellow_checkmark.svg.png"
+        />
+        <img
+          onClick={() => removeTask(index)}
+          alt="yoyo"
+          className="h-6 py-auto m-1 cursor-pointer"
+          src="https://cdn-icons-png.freepik.com/512/210/210135.png"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Task;
---
+++ src/component/Todolist.js
@@ -0,0 +1,72 @@
+import React from "react";
+import Task from "./Task";
+import { useState } from "react";
+
+const Todolist = () => {
+  const defaultTask = [
+    {
+      title: "Grab some Pizza",
+      completed: false,
+    },
+  ];
+
+  const [items, setItems] = useState(defaultTask);
+  const [newItem, setNewItem] = useState("");
+
+  const addTask = () => {
+    setItems([...items, { title: newItem, completed: false }]);
+  };
+
+  const completeTask = (index) => {
+    const newTasks = [...items];
+    newTasks[index].completed = true;
+    setItems(newTasks);
+  };
+
+  const removeTask = (index) => {
+    const newTasks = [...items];
+    newTasks.splice(index, 1);
+    setItems(newTasks);
+  };
+
+  return (
+    <div className=" bg-black w-4/12 h-auto flex-row rounded-2xl text-white">
+      <h1 className="text-3xl p-5 text-center">TODO - ITEMS!</h1>
+
+      <div className="m-15 p-10">
+        <ul className="content-center">
+          {items.map((task, index) => (
+            <Task
+              task={task}
+              key={index}
+              index={index}
+              completeTask={completeTask}
+              removeTask={removeTask}
+            />
+          ))}
+        </ul>
+      </div>
+
+      <div className="flex items-center justify-center">
+        <input
+          className="text-black my-5 mx-2 py-1 rounded-lg border-blue-500"
+          type="text"
+          placeholder=" Add new item here"
+          value={newItem}
+          onChange={(e) => {
+            setNewItem(e.target.value);
+          }}
+        />
+        <button
+          className="bg-yellow-100 text-black rounded-lg px-2 py-1 my-5"
+          onClick={addTask}
+        >
+          {" "}
+          Add new item
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Todolist;
